e9aa10d863fdade8e8876948e4ef0fd2bb725ef9
Ceren <117370778+ceren15@users.noreply.github.com>
2025-07-30 10:08:13 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class", // darkMode aktifleştirme
     content: [
         "./src/**/*.{js,ts,jsx,tsx}",    // src klasörü
         "./app/**/*.{js,ts,jsx,tsx}",    // app klasörü
